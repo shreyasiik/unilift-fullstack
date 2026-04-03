3630afd909de72e729eca58edd41fbe7741ada54
Shreyasi <shreyasi.kathait6@gmail.com>
2026-04-03 22:22:57 +0530

    cors update server.js2

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
+const app = express();
 app.use(cors({
   origin: "https://unilift.vercel.app", // ✅ your frontend URL
   credentials: true,
